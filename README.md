3d33b05dbe2dcfa74a7424e47618f6436e0c4c91
Jonathan Disla <jond@Jonathans-MacBook-Air.local>
2021-01-07 15:56:21 -0500

    up 1.2

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # web-340
 Node JS
+# Contributors
+# Contributors
+- Professor Massoud - Bellevue University
+- Jonathan Disla - Bellevue University
---
+++ week-1/disla-exercise-1.2.js
@@ -0,0 +1,9 @@
+var url = require("url");
+
+var parsedURL = url.parse("https://www.example.com/profile?name=jonathan");
+
+console.log(parsedURL.protocol);
+
+console.log(parsedURL.host);
+
+console.log(parsedURL.query);
