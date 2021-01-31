ca9fc4403ab2cf3cc6cd21e29fc6dc753f5f42d5
Jonathan Disla <jon@Jonathans-Mac-mini.local>
2021-01-31 13:48:43 -0500

    up 4.2

---
+++ week-4/json-api/disla-exercise4.2.js
@@ -0,0 +1,21 @@
+//requiring the below libraries
+var express = require('express');
+var http = require('http');
+var app = express();
+
+//create route /customer with the paramenter id
+app.get("/customer/:id", (req, res)=>{
+    //store the id as an integer in the id variable
+    var id = parseInt(req.params.id, 10);
+    //cfeate json object that receives the id of the api call
+    res.json({
+        firstName: "John",
+        lastName: "Disla",
+        employeeId: id
+    })
+})
+
+//create the server on port 8008
+http.createServer(app).listen(8080, ()=>{
+    console.log("Listening on port: " + 8080)
+})
