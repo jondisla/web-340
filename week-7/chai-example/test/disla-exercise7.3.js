1062f627b87c1064f566d26baad5dba41dfbee2b
Jonathan Disla <jon@Jonathans-Mac-mini.local>
2021-02-21 20:36:35 -0500

    up 7.3

---
+++ week-7/chai-example/test/disla-exercise7.3.js
@@ -0,0 +1,11 @@
+var fruits = require("../disla-fruits");
+var chai = require("chai");
+var assert = chai.assert;
+
+describe("fruits", function () {
+  it("should return an array of fruits", function () {
+    var f = fruits("Apple,Orange,Mango");
+
+    assert(Array.isArray(f));
+  });
+});
